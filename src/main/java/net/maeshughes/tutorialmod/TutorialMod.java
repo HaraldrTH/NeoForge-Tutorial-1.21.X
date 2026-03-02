05a0b4c42867a3aefde931ba7d20cce218cbaa1e
Harald Turøy Hansen <haraldth@student.uia.no>
2026-03-02 20:08:04 +0100

    Added 1 more item

--- src/main/java/net/maeshughes/tutorialmod/TutorialMod.java
+++ src/main/java/net/maeshughes/tutorialmod/TutorialMod.java
@@ -71,6 +71,7 @@ public class TutorialMod {
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
             event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
         }
     }
 
