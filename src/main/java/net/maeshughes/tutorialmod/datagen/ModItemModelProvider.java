defb00981918a78bc5d2d04777f1a1b3d95451f8
Harald Turøy Hansen <haraldth@student.uia.no>
2026-03-13 21:06:27 +0100

    Backed up Data, and implemented DataGenerators.

---
+++ src/main/java/net/maeshughes/tutorialmod/datagen/ModItemModelProvider.java
@@ -0,0 +1,25 @@
+package net.maeshughes.tutorialmod.datagen;
+
+import net.maeshughes.tutorialmod.TutorialMod;
+import net.maeshughes.tutorialmod.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends ItemModelProvider {
+
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        basicItem(ModItems.BISMUTH.get());
+        basicItem(ModItems.RAW_BISMUTH.get());
+
+        basicItem(ModItems.RADISH.get());
+        basicItem(ModItems.STARLIGHT_ASHES.get());
+        basicItem(ModItems.FROSTFIRE_ICE.get());
+        basicItem(ModItems.CHISEL.get());
+    }
+}
