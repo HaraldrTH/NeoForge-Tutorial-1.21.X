a4caec1796faec30f2c124f4dbbb44cd9ce99379
Harald Turøy Hansen <haraldth@student.uia.no>
2026-03-23 18:47:26 +0100

    Added non-block blocks like door, trapdoor, fence, gate, button, stairs, slabs, pressure plate etc.

--- src/main/java/net/maeshughes/tutorialmod/datagen/ModBlockTagProvider.java
+++ src/main/java/net/maeshughes/tutorialmod/datagen/ModBlockTagProvider.java
@@ -21,10 +21,14 @@ public class ModBlockTagProvider extends BlockTagsProvider {
         tag(BlockTags.MINEABLE_WITH_PICKAXE)
                 .add(ModBlocks.BISMUTH_BLOCK.get())
                 .add(ModBlocks.BISMUTH_ORE.get())
-                .add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get())
-                .add(ModBlocks.MAGIC_BLOCK.get());
+                .add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get());
         tag(BlockTags.NEEDS_IRON_TOOL)
                 .add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get())
-                .add(ModBlocks.BISMUTH_ORE.get());
+                .add(ModBlocks.BISMUTH_ORE.get())
+                .add(ModBlocks.MAGIC_BLOCK.get());
+
+        tag(BlockTags.FENCES).add(ModBlocks.BISMUTH_FENCE.get());
+        tag(BlockTags.FENCE_GATES).add(ModBlocks.BISMUTH_FENCE_GATE.get());
+        tag(BlockTags.WALLS).add(ModBlocks.BISMUTH_WALL.get());
     }
 }
